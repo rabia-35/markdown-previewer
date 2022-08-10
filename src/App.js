e426ee2487c6d29c7322f2a97150728cac0fdf14
rabia ölker <rabia.olker@gmail.com>
2022-08-10 17:43:15 +0300

    exported files to appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import Markdown from './components/markdown';
 import './App.css';
+import Header from './components/header';
+import { MarkdownPa } from './context/markdownContext';
 
 function App() {
   return (
     <div className="App">
-      
+      <MarkdownPa>
+        <Header />
+        <Markdown />
+       </MarkdownPa>
     </div>
   );
 }
