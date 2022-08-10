220344b5c32dd53e5305c99f92d27b21b9099d63
rabia <rabia.olker@gmail.com>
2022-08-10 21:08:01 +0300

    pulled states from markdownContext

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useMarkdown } from '../context/markdownContext';
 import { Container } from 'react-bootstrap';
+import { marked } from "marked";
 
 function Header() {
+  const {text, setText}=useMarkdown();
     const help = `Heading
 =======
 
@@ -35,10 +38,17 @@ The rain---not the reign---in
 Spain.
 
  *[Herman Fassett](https://freecodecamp.com/hermanfassett)*`;
+
+const handleClick=()=>{
+  setText(help)
+
+}
+console.log(text)
+
   return (
     <Container className='d-flex justify-content-between'>
             <h2 className='title'>Markdown Previewer</h2>
-            <div>
+            <div >
                  <svg
                   xmlns="http://www.w3.org/2000/svg" 
                   role="button" 
@@ -47,6 +57,7 @@ Spain.
                   fill="currentColor"
                   className="bi bi-question-square-fill"
                   viewBox="0 0 16 16"
+                  onClick={handleClick}
                     >
                      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z"/>
                  </svg>
